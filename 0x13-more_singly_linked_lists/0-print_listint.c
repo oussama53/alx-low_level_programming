24ea62e5523af5fd29417cd5a3527fde911c4a02
oussama53 <oussama.elbkari@gmail.com>
2023-10-23 08:43:17 -0700

    hello

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,14 +5,14 @@
  * 
  * Return: size of lists
  */
- size_t print_listint(const listint_t *h)
- {
-     size_t i = 0;
-     while (h)
-     {
-         printf("%d\n" , h->n);
+size_t print_listint(const listint_t *h)
+{
+    size_t i = 0;
+    while (h)
+    {
+        printf("%d\n" , h->n);
          h = h->next;
          i++;
-     }
-     return (i);
- }
+    }
+    return (i);
+}
